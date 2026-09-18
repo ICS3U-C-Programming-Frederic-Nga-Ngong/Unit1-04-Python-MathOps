8e04a993194d0a9abf83b7e801bf33cdf4953ad3
Frederic nga-ngong <Frederic.nga-ngong@stu.ocsb.ca>
2026-09-18 13:45:04 +0000

    code performingbasic math operation in basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created By: frederic
+# Date: feb 2008 18
+# performs basic math operations
+def main():
+    print("20+5={}".format(20 + 5))
+    print("20-5={}".format(20 - 5))
+    print("20*5={}".format(20 * 5))
+    print("20/5={}".format(20 / 5))
+    print("2**5={}".format(2**5))
+    print("21%5={}".format(21 % 5))
+
+
+if __name__ == "__main__":
+
+    main()
